98917ba37c04519689a79959279b5a9c88887996
fariaspio <fariaspio@hotmail.com>
2016-04-30 18:20:06 -0300

    Adicionando porjeto

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('cartCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('aPLICATIVO_ProdutoCtrl', function($scope) {
+
+})
+   
+.controller('SERVIOCURSOCtrl', function($scope) {
+
+})
+   
+.controller('eTECEMPRESACtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,89 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/PAGINA INICIAL',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.cart', {
+    url: '/page2',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cart.html',
+        controller: 'cartCtrl'
+      }
+    }
+  })
+
+  .state('menu.cloud', {
+    url: '/page3',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cloud.html',
+        controller: 'cloudCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page4',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('menu.aPLICATIVO_Produto', {
+    url: '/produto_descrição',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/aPLICATIVO_Produto.html',
+        controller: 'aPLICATIVO_ProdutoCtrl'
+      }
+    }
+  })
+
+  .state('menu.SERVIOCURSO', {
+    url: '/curso_descrição',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/SERVIOCURSO.html',
+        controller: 'SERVIOCURSOCtrl'
+      }
+    }
+  })
+
+  .state('menu.eTECEMPRESA', {
+    url: '/empresa_etec',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/eTECEMPRESA.html',
+        controller: 'eTECEMPRESACtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/PAGINA INICIAL')
+
+  
+
+});
